9f3e84d9f2b90719cc93f484a4268601b460c0af
Mervyn Vangenechten <mervynvangenechten@gmail.com>
2026-01-24 00:19:42 +0100

    --

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,13 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+
+function redirectToPage() {
+    const selectedPage = document.getElementById('destination').value;
+
+    if (selectedPage) {
+        window.location.href = selectedPage;
+    } else {
+        alert("Gelieve een bestemming te kiezen.");
+    }
+}
